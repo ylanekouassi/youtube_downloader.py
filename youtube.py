f0cdb1f75353c4b1d81c92cd40c429a8fd369ebb
ylanekouassi <121358143+ylanekouassi@users.noreply.github.com>
2024-03-08 21:44:33 -0500

    stream variable created for the video quality

--- youtube.py
+++ youtube.py
@@ -2,4 +2,8 @@
 
 import pytube
 
-yt = pytube.Youtube()
+url = input("Enter your URL: ")
+yt = pytube.YouTube(url) #need a url as a parameter
+
+stream = yt.streams.get_highest_resolution()
+stream.download()
